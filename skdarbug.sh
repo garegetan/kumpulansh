84bdf61dfe42e4eee2ef2452bf67f6d220ca02d2
garegetan <79066977+garegetan@users.noreply.github.com>
2021-02-15 11:23:15 +0700

    Create skdarbug.sh

---
+++ skdarbug.sh
@@ -0,0 +1,105 @@
+#!/bin/bash
+
+green='\33[32;1m'
+blue='\33[0;36m'
+red='\33[31;1m'
+purple='\e[0;35m'
+Oren='\33[33;1m'
+black='\33[30;1m'
+white='\33[37;1m'
+yellow='\33[33;1m'
+
+clear
+
+toilet -f mini -F gay FAUZAN CK
+echo
+sleep 2
+clear
+
+echo "
+   =======================================
+    SELAMAT DATANG DI TOOLS FAUZAN PRATAMA
+   =======================================
+" | lolcat
+sleep 2
+clear
+blue='\033[34;1m'
+green='\033[32;1m'
+purple='\033[35;1m'
+cyan='\033[36;1m'
+red='\033[31;1m'
+white='\033[37;1m'
+yellow='\033[33;1m'
+sleep 1
+toilet -f big -F gay password
+echo "\033[32;1m+=============================+"
+echo "\033[32;1mSILAKAN MASUKAN PW :fauzanpratama:"
+echo "\033[32;1mTelegram : t.me/xzuan"
+echo "\033[32;1mWhatsapp : 082217836017"
+echo "\033[32;1m+=============================+"
+
+name="fauzanpratama"
+while [ "$adin" != "$name" ]
+do
+  read -p "MASUKAN PASSWORD NGAB:)" adin
+done
+  echo "OK PASSWORD ANDA BENAR..."
+  sleep 2
+  clear
+  
+  blue='\033[34;1m'
+green='\033[32;1m'
+purple='\033[35;1m'
+cyan='\033[36;1m'
+red='\033[31;1m'
+white='\033[37;1m'
+yellow='\033[33;1m'
+sleep 1
+toilet -f big -F gay welcome
+
+echo $yellow"======================================="
+echo $yellow"      AUTHOR : FAUZAN PRATAMA"
+echo $yellow"      KONTAK  : 081276754581"
+echo $yellow"======================================="
+echo $green "======================================="
+echo $green" [ 01 ] BUG TSEL WHATSAPP"
+echo $green" [ 02 ] BUG TSEL YOUTUBE"
+echo $green" [ 03 ] BUG TSEL INSTAGRAM"
+echo $green" [ 04 ] BUG AXIS GAME"
+echo $green" [ 05 ] BUG TSEL KB BIASA"
+echo $green" [ 00 ] EXIT"
+echo $green"========================================"
+ echo $white" PILIH MENU DI ATAS"
+ read -p " [ PILIH MENU ]>>>)" pilih ;
+ if [ $pilih = "01" ]
+ then
+ clear
+ toilet -f big -F gay Bug wa
+ echo $green"Loading...."
+ sleep 2
+ echo $red"================"
+ echo $red"www.whatsapp.com"
+ echo $red"================"
+ fi
+ if [ $pilih = "02" ]
+ then
+ clear
+ toilet -f big -F gay Bug Yt
+ echo $green"Loading...."
+ sleep 2
+ echo $red"============="
+ echo $red"m.youtube.com"
+ echo $red"============="
+ fi
+ if [ $pilih = "03" ]
+ then
+ clear
+ toilet -f big -F gay Bug ig
+ echo $green"Loading...."
+ sleep 2
+ echo $red"=============="
+ echo $red"m.instagram.com"
+ echo $red"=============="
+ fi
+ 
+  
